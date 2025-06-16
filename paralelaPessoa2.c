2736b43952a53a7eeef9dcb8c5d2568773e87b6e
vviivviaan <viviangomess2002@gmail.com>
2025-06-16 17:02:08 -0300

    prog serial ajustada de py para c

---
+++ paralelaPessoa2.c
@@ -0,0 +1,4 @@
+void calcCoordenadas(int IDbloco, int *primeira_linha, int *ultima_linha, int *primeira_coluna, int *ultima_coluna)
+{
+
+}
---
+++ serial.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define largura_matriz 3 //aqui vc define a largura que quer que sua matriz tenha
+#define altura_matriz 3 // e aqui, a altura
+
+int ehPrimo(int n){
+    if (n < 2){
+        return 0;
+    }
+    int i = 2;
+    while (i <= (int)sqrt(n)){
+        if (n % i == 0){
+            return 0; //aqui retorna False porque achou divisor
+        }
+        i++;
+    }
+    return 1; // True se não achou divisor
+}
+
+//inicializando e printando a matriz
+void imprimirMatriz (int matriz[altura_matriz][largura_matriz]){
+    printf("A matriz eh: \n");
+    for (int i = 0; i < altura_matriz; i++){
+        for (int j = 0; j < largura_matriz; j++){
+            matriz[i][j] = rand() % 4000;
+            printf("%4d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int contaPrimos(int matriz[altura_matriz][largura_matriz]){
+    int n_primos = 0;
+    printf("\nNumeros primos encontrados na matriz:\n");
+    for (int altura = 0; altura < altura_matriz; altura++){
+        for (int largura = 0; largura < largura_matriz; largura++){
+            int n = matriz[altura][largura];
+            if (ehPrimo(n)){
+                n_primos++;
+                printf("%d\n", n);
+            }
+        }
+    }
+    return n_primos;
+}
+
+int main() {
+    int matriz_principal[altura_matriz][largura_matriz];
+
+    srand(time(NULL)); //gera numeros aleatorios
+    imprimirMatriz(matriz_principal);
+    int n_primos = contaPrimos(matriz_principal);
+
+    printf("\nTotal de primos encontrados: %d\n", n_primos);
+
+    return 0;
+}
